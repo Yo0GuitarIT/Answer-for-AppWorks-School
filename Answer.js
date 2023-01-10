21937533ec013f6effba943897d63f5f15a293d8
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-01-11 00:17:23 +0800

    Update Answer.js

--- Answer.js
+++ Answer.js
@@ -1,17 +1,17 @@
-//開始執行程式
-var x = 0 //設定一個初始變數值為０ 放入x當中
+//Start the program
+var x = 0 //At the beginning, set an initial value variable 0 into x.
 
 while (x*x<36){
   x+=1
 }
 
 /*
-放入x*x<36的條件去檢視是否成立。
-如果成立，x值將會加１，變成x=1，放入條件繼續檢視。
+Put in the condition of x*x<36 to check whether it is true.
+If it is true, the x value will be increased by 1 to become x=1, put in the condition and continue to check.
+If the condition of x*x<36 continues to hold true, x will continue to be incremented by 1 and put into view. 
+Until the conditions are not met (here, the case of x*x≥36), the value of x is printed out directly.
 
-如果x*x<36的條件持續成立的狀況下，x將繼續被加１放入檢視。
-直到不符合條件情況時（在此即為x*x≥36）<跳出迴圈。
 */
 
-console.log(x) //印出x值
-//結束程式
+console.log(x) //print x
+//End the program
